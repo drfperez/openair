7b660c7adddecf4b6b7b15cd41ba92c6f30b213f
Francisco Pérez <drfperez@gmail.com>
2022-04-12 16:10:27 +0200

    Update datamanipulation.R

--- datamanipulation.R
+++ datamanipulation.R
@@ -12,7 +12,6 @@ city2<-city1[-c(1,2,4,6:16)]
 # You need dplyr library to use pipe symbol %>% and combine two columns in one
 library(dplyr)
 city2 <- city2 %>% mutate(date=paste0(data, " ", hour))
-
 # check the names of your columns
 colnames (city2)
 # delete some unnecessary columns
@@ -29,11 +28,4 @@ class (city2)
 city2 <- as_data_frame(city2)
 # call lares library to find and replace some values including NA
 library (lares)
-
 city2<-replaceall(city2, c("T00:00:00.000 h01", "T00:00:00.000 h02","T00:00:00.000 h03","T00:00:00.000 h04","T00:00:00.000 h05","T00:00:00.000 h06","T00:00:00.000 h07","T00:00:00.000 h08","T00:00:00.000 h09","T00:00:00.000 h10","T00:00:00.000 h11","T00:00:00.000 h12","T00:00:00.000 h13","T00:00:00.000 h14","T00:00:00.000 h15","T00:00:00.000 h16","T00:00:00.000 h17","T00:00:00.000 h18","T00:00:00.000 h19","T00:00:00.000 h20","T00:00:00.000 h21","T00:00:00.000 h22","T00:00:00.000 h23","T00:00:00.000 h24"), c(" 01:00:00", " 02:00:00", " 03:00:00", " 04:00:00"," 05:00:00", " 06:00:00"," 07:00:00", " 08:00:00"," 09:00:00", " 10:00:00"," 11:00:00", " 12:00:00"," 13:00:00", " 14:00:00"," 15:00:00", " 16:00:00"," 17:00:00", " 18:00:00"," 19:00:00", " 20:00:00"," 21:00:00", " 22:00:00"," 23:00:00", " 00:00:00"))
-
-
-
-
-
-
